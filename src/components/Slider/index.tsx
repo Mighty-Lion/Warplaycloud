93fae1badf99f494e4c42b1d45d95b0806a2984a
Mighty-Lion <79599003+Mighty-Lion@users.noreply.github.com>
2023-07-02 23:37:56 +0300

    Update slider settings

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -3,13 +3,11 @@ import { SliderContainer } from '@/components/Slider/index.styles';
 
 const autoPlaySettings = {
   dots: false,
-  arrows: false,
+  arrows: true,
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  pauseOnHover: true,
+  
 };
 export function SliderAutoPlay({ children }: PropsWithChildren) {
   return <SliderContainer {...autoPlaySettings}>{children}</SliderContainer>;
