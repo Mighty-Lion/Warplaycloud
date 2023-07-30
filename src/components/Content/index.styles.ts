a6014f7180462905296f652c733bb5aa9d7880d3
Aslan Abaev <hedgehog.asl@yandex.ru>
2023-07-30 20:22:51 +0300

    deleted test command

--- src/components/Content/index.styles.ts
+++ src/components/Content/index.styles.ts
@@ -1,5 +1,10 @@
 import styled from '@emotion/styled';
+import { getBreakpoint } from '@/Breakpoints';
 
 export const ContentContainer = styled.div`
   border-left: 1px solid var(--color-gray-500);
+
+  @media (max-width: ${getBreakpoint('TABLET_UL', 'down')}) {
+    border-left: none;
+  }
 `;
