403813cb87bc82d41ac0ff057cf05fb6b6b4afc3
Aslan Abaev <hedgehog.asl@yandex.ru>
2023-08-11 16:05:35 +0300

    fix Content

--- src/components/Content/index.styles.ts
+++ src/components/Content/index.styles.ts
@@ -5,7 +5,7 @@ export const ContentContainer = styled.div`
   border-left: 1px solid var(--color-gray-500);
 	padding-bottom: 215px;
 
-  @media (max-width: ${getBreakpoint('TABLET_UL', 'down')}) {
+  @media (max-width: ${getBreakpoint('DESKTOP_S', 'down')}) {
     border-left: none;
   }
 `;
