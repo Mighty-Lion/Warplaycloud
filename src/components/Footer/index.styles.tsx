19d9f01af8cac43338284a8f3955db0a07dc9d73
Aslan Abaev <hedgehog5349@gmail.com>
2023-08-13 16:43:12 +0300

    dev Footer

--- src/components/Footer/index.styles.tsx
+++ src/components/Footer/index.styles.tsx
@@ -1,3 +1,80 @@
 import styled from '@emotion/styled';
+import exp from 'constants';
+import { Link } from "react-router-dom";
 
-export const FooterWrapper = styled.div``;
+export const FooterWrapper = styled.footer`
+  width: 100vw;
+  background: var(--color-black-800);
+`;
+
+export const FooterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 56px 66px 56px 150px;
+  width: 100%;
+  max-width: 1440px;
+  background: var(--color-black-800);
+`;
+
+export const FooterLogos = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const FooterLogo = styled.img``;
+
+export const FooterAuthorImg = styled.img`
+  margin-left: 29px;
+`;
+export const FooterDesignImg = styled.img`
+  margin-left: 20px;
+`;
+
+export const FooterLinks = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const FooterContacts = styled(Link)`
+  color: #838383;
+  font-family: Roboto, sans-serif;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 200;
+  line-height: normal;
+`;
+
+export const FooterPolitics = styled.a`
+  color: #838383;
+  font-family: Roboto, sans-serif;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 200;
+  line-height: normal;
+`;
+
+export const FooterCookie = styled.a`
+  margin-left: 51px;
+  color: #838383;
+  font-family: Roboto, sans-serif;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 200;
+  line-height: normal;
+`;
+
+export const FooterSocials = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+  margin-left: 51px;
+`;
+
+export const FooterSocialLink = styled.a ` 
+  display: flex;
+`
+
+export const FooterSocialImg = styled.img ` 
+  
+`
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,50 @@
-import { FooterWrapper } from '@/components/Footer/index.styles';
+import {
+  FooterAuthorImg,
+  FooterContainer,
+  FooterCookie,
+  FooterDesignImg,
+  FooterLinks,
+  FooterLogo,
+  FooterLogos,
+  FooterPolitics,
+  FooterSocialImg,
+  FooterSocialLink,
+  FooterSocials,
+  FooterWrapper,
+} from '@/components/Footer/index.styles';
+
+import Logo from '@/assets/images/svg/footer-logo.svg';
+import Author from '@/assets/images/svg/author.svg';
+import Design from '@/assets/images/svg/design.svg';
+import VK from '@/assets/images/svg/vk-white.svg';
+import Discord from '@/assets/images/svg/discord-white.svg';
+import Youtube from '@/assets/images/svg/youtube-white.svg';
 
 export function Footer() {
-  return <FooterWrapper />;
+  return (
+    <FooterWrapper>
+      <FooterContainer>
+        <FooterLogos>
+          <FooterLogo src={Logo} alt="logo" />
+          <FooterAuthorImg src={Author} alt="author logo" />
+          <FooterDesignImg src={Design} alt="designer logo" />
+        </FooterLogos>
+        <FooterLinks>
+          <FooterPolitics href="#">Политика конфиденциальности</FooterPolitics>
+          <FooterCookie href="#">Файлы Cookie</FooterCookie>
+          <FooterSocials>
+            <FooterSocialLink href="#">
+              <FooterSocialImg src={VK} alt="vk logo" />
+            </FooterSocialLink>
+            <FooterSocialLink href="#">
+              <FooterSocialImg src={Discord} alt="discord logo" />
+            </FooterSocialLink>
+            <FooterSocialLink href="#">
+              <FooterSocialImg src={Youtube} alt="youtube logo" />
+            </FooterSocialLink>
+          </FooterSocials>
+        </FooterLinks>
+      </FooterContainer>
+    </FooterWrapper>
+  );
 }
