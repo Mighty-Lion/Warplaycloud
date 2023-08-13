924b8b532f5541839f33300ea04b65b66e554e5a
Aslan Abaev <hedgehog5349@gmail.com>
2023-08-13 15:17:59 +0300

    dev Locations

--- src/components/Locations/index.styles.ts
+++ src/components/Locations/index.styles.ts
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import { motion } from 'framer-motion';
+import { getBreakpoint } from '@/Breakpoints';
 
 export const LocationsContainer = styled.div`
   display: flex;
@@ -8,25 +9,32 @@ export const LocationsContainer = styled.div`
   justify-content: center;
 `;
 export const LocationsThemeWrapper = styled.div`
-  margin-right: 50px;
+  margin-left: 50px;
+  margin-bottom: 5px;
 `;
 
-export const LocationsSvgWrapper = styled(motion.div)`
+export const LocationsSvgWrapper = styled.div`
   width: 100%;
-  max-width: 500px;
+  max-width: 731px;
+
+  @media (max-width: ${getBreakpoint('TABLET_UL', 'down')}) {
+    width: calc(100% + 205px);
+    transform: translateY(-5px);
+  }
 `;
 
 export const LocationsImg = styled(motion.img)`
   width: 100%;
   object-fit: cover;
-  fill: radial-gradient(
-      311% 92.28% at 6.37% 87.35%,
-      rgba(255, 255, 255, 0.05) 0%,
-      rgba(255, 255, 255, 0.05) 0%,
-      rgba(255, 255, 255, 0) 100%,
-    ),
-    #161d25;
   filter: drop-shadow(-17px 30px 22px rgba(16, 16, 16, 0.49))
     drop-shadow(0px 10px 30px rgba(0, 197, 255, 0.3))
     drop-shadow(0px 5px 5px #00c5ff) drop-shadow(0px 4px 0px #1a1a1a);
+  transition: 0.5s all;
+
+  &:hover {
+    filter: drop-shadow(-17px 30px 22px rgba(16, 16, 16, 0.49))
+      drop-shadow(0px 10px 30px rgba(0, 197, 255, 0.3))
+      drop-shadow(0px 5px 5px var(--color-white-0))
+      drop-shadow(0px 4px 0px #1a1a1a);
+  }
 `;
