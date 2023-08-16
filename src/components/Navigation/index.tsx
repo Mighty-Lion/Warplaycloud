3a5ed3593d394e011a044291e40ea32e462a10cd
Aslan Abaev <hedgehog5349@gmail.com>
2023-08-16 19:06:45 +0300

    dev handleScrolling

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,29 +1,37 @@
-import { useState } from 'react';
+import { MutableRefObject, useState } from 'react';
 import { NavBtn } from '@/components/Navigation/partials/NavBtn';
 import { Nav, NavTab } from '@/components/Navigation/index.styles';
-
-const tabs = [
-  { id: '0', label: 'Главная', to: '/' },
-  { id: '1', label: 'Сервера', to: '/' },
-  { id: '2', label: 'Скачать', to: '/' },
-  { id: '3', label: 'Открыть в браузере', to: '/' },
-  { id: '4', label: 'Цены', to: '/' },
-  { id: '5', label: 'Тех поддержка', to: '/' },
-  { id: '6', label: 'Контакты', to: '/' },
-];
+import { useAllRef } from '@/hooks/useAllRef';
 
 export interface INavBtnProps {
   onClick?: () => void;
   to: string;
+  pointer?: MutableRefObject<null>;
 }
 
 interface INavigationProps {
   tabId: string;
 }
+interface IHandleScrollingProps {
+  id: string;
+  pointer?: MutableRefObject<null>;
+}
+
 export function Navigation({ tabId }: INavigationProps) {
+  const { refCloud, refBegin, refRates, refDevices } = useAllRef();
+  const tabs = [
+    { id: '0', label: 'Главная', to: '/', pointer: refCloud },
+    { id: '1', label: 'Сервера', to: '/', pointer: refCloud },
+    { id: '2', label: 'Скачать', to: '/', pointer: refBegin },
+    { id: '3', label: 'Открыть в браузере', to: '/', pointer: refDevices },
+    { id: '4', label: 'Цены', to: '/', pointer: refRates },
+    { id: '5', label: 'Тех поддержка', to: '/', pointer: refCloud },
+    { id: '6', label: 'Контакты', to: '/', pointer: refCloud },
+  ];
+
   const [activeTab, setActiveTab] = useState(tabs[0].id);
 
-  const handleScrolling = (id: string) => {
+  const handleScrolling = (id: string, pointer: MutableRefObject<null>) => {
     setActiveTab(id);
     window.scrollTo({ top: 0 });
   };
@@ -34,7 +42,7 @@ export function Navigation({ tabId }: INavigationProps) {
         <NavBtn
           to={tab.to}
           key={tab.id}
-          onClick={() => handleScrolling(tab.id)}
+          onClick={() => handleScrolling(tab.id, tab.pointer)}
         >
           {activeTab === tab.id && (
             <NavTab
