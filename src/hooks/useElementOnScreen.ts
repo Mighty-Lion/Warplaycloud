a5b20172a5f586973a19cdc16dc61f4894cda14e
Aslan Abaev <hedgehog.asl@yandex.ru>
2023-12-03 15:06:40 +0300

    added useElementOnScreen

---
+++ src/hooks/useElementOnScreen.ts
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+
+export function useElementOnScreen(
+  options: IntersectionObserverInit | undefined,
+  containerRef: any
+) {
+  function callbackFunction(entries) {
+    const [entry] = entries;
+    // console.log(entry);
+  }
+
+  useEffect(() => {
+    const currentRef = containerRef.current;
+    const observer = new IntersectionObserver(callbackFunction, options);
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, [containerRef, options]);
+
+  return { containerRef };
+}
