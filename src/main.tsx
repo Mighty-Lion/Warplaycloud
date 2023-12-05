8ae364b7ac232757294322416cd3a65111b9d4e3
Aslan Abaev <hedgehog5349@gmail.com>
2023-12-05 17:33:09 +0300

    dev main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,11 +10,13 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './css/colors.css';
 import ScrollToTop from "@/components/ScrollToTop";
+import { ScrollToAnchor } from "@/components/ScrollToAnchor";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
       <ScrollToTop />
+      <ScrollToAnchor />
       <App />
     </Router>
     <ToastContainer
