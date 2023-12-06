dd66da5d3682f1a5fb1a7ea3fa50686ea60d0ff9
Aslan Abaev <hedgehog5349@gmail.com>
2023-12-06 17:50:31 +0300

    dev Navigation tabs logic

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,11 +1,11 @@
 import { MutableRefObject, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useInView } from 'framer-motion';
 import { NavBtn } from '@/components/Navigation/partials/NavBtn';
 import { Nav, NavTab } from '@/components/Navigation/index.styles';
 import { useSideMenu } from '@/hooks/useSideMenu';
 import { useAllRef } from '@/hooks/useAllRef';
 import useElementOnScreen from '@/hooks/useElementOnScreen';
-import { useInView } from "framer-motion";
 
 export interface INavBtnProps {
   onClick?: () => void;
@@ -26,7 +26,7 @@ export function Navigation({ tabId }: INavigationProps) {
     { id: '2', label: 'Скачать', to: '/#download' },
     { id: '3', label: 'Цены', to: '/#prices' },
     { id: '4', label: 'Открыть в браузере', to: '/#devices' },
-    { id: '5', label: 'Тех поддержка', to: '/' },
+    { id: '5', label: 'Тех поддержка', to: '/support' },
     { id: '6', label: 'Контакты', to: '/contacts' },
   ];
 
@@ -34,16 +34,46 @@ export function Navigation({ tabId }: INavigationProps) {
 
   const location = useLocation();
 
-  const isInSightSlider = useElementOnScreen(refSlider, '/' || '/#main');
-  const isInViewSlider = useInView(refSlider);
-  const isInViewCloud = useInView(refCloud);
-  useEffect(() => {
-    if (isInViewSlider) setActiveTab(tabs[0].id);
-  }, [isInViewSlider]);
+  const isInSightSlider = useElementOnScreen(refSlider, '/');
+  const isInSightCloud = useElementOnScreen(refCloud, '/');
+  const isInSightBegin = useElementOnScreen(refBegin, '/');
+  const isInSightRates = useElementOnScreen(refRates, '/');
+  const isInSightDevices = useElementOnScreen(refDevices, '/');
 
   useEffect(() => {
-    if (isInViewCloud) setActiveTab(tabs[1].id);
-  }, [isInViewCloud]);
+    if (isInSightSlider) {
+      setActiveTab(tabs[0].id);
+      history.pushState({}, '', '#main');
+    }
+  }, [isInSightSlider]);
+
+  useEffect(() => {
+    if (isInSightCloud) {
+      setActiveTab(tabs[1].id);
+      history.pushState({}, '', '#cloud');
+    }
+  }, [isInSightCloud]);
+
+  useEffect(() => {
+    if (isInSightBegin) {
+      setActiveTab(tabs[2].id);
+      history.pushState({}, '', '#download');
+    }
+  }, [isInSightBegin]);
+
+  useEffect(() => {
+    if (isInSightRates) {
+      setActiveTab(tabs[3].id);
+      history.pushState({}, '', '#prices');
+    }
+  }, [isInSightRates]);
+
+  useEffect(() => {
+    if (isInSightDevices) {
+      setActiveTab(tabs[4].id);
+      history.pushState({}, '', '#devices');
+    }
+  }, [isInSightDevices]);
 
   useEffect(() => {
     if (location.pathname === '/contacts') setActiveTab(tabs[6].id);
