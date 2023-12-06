a5320cfdac4974e96a6fd644fdfe219a092e25cd
Aslan Abaev <hedgehog.asl@yandex.ru>
2023-12-06 22:09:42 +0300

    dev Navigation

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,10 +1,9 @@
-import { MutableRefObject, useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { MutableRefObject } from 'react';
 import { NavBtn } from '@/components/Navigation/partials/NavBtn';
 import { Nav, NavTab } from '@/components/Navigation/index.styles';
 import { useSideMenu } from '@/hooks/useSideMenu';
 import { useAllRef } from '@/hooks/useAllRef';
-import useElementOnScreen from '@/hooks/useElementOnScreen';
+import useChangeUrl from '@/hooks/useChangeUrl';
 
 export interface INavBtnProps {
   onClick?: () => void;
@@ -29,54 +28,13 @@ export function Navigation({ tabId }: INavigationProps) {
     { id: '6', label: 'Контакты', to: '/contacts' },
   ];
 
-  const [activeTab, setActiveTab] = useState(tabs[0].id);
+  useChangeUrl(refSlider, '/', '/#main');
+  useChangeUrl(refCloud, '/', '/#cloud');
+  useChangeUrl(refBegin, '/', '/#download');
+  useChangeUrl(refRates, '/', '/#prices');
+  useChangeUrl(refDevices, '/', '/#devices');
 
-  const location = useLocation();
-
-  const isInSightSlider = useElementOnScreen(refSlider, '/');
-  const isInSightCloud = useElementOnScreen(refCloud, '/');
-  const isInSightBegin = useElementOnScreen(refBegin, '/');
-  const isInSightRates = useElementOnScreen(refRates, '/');
-  const isInSightDevices = useElementOnScreen(refDevices, '/');
-
-  useEffect(() => {
-    if (isInSightSlider) {
-      setActiveTab(tabs[0].id);
-      history.pushState({}, '', '#main');
-    }
-  }, [isInSightSlider]);
-
-  useEffect(() => {
-    if (isInSightCloud) {
-      setActiveTab(tabs[1].id);
-      history.pushState({}, '', '#cloud');
-    }
-  }, [isInSightCloud]);
-
-  useEffect(() => {
-    if (isInSightBegin) {
-      setActiveTab(tabs[2].id);
-      history.pushState({}, '', '#download');
-    }
-  }, [isInSightBegin]);
-
-  useEffect(() => {
-    if (isInSightRates) {
-      setActiveTab(tabs[3].id);
-      history.pushState({}, '', '#prices');
-    }
-  }, [isInSightRates]);
-
-  useEffect(() => {
-    if (isInSightDevices) {
-      setActiveTab(tabs[4].id);
-      history.pushState({}, '', '#devices');
-    }
-  }, [isInSightDevices]);
-
-  useEffect(() => {
-    if (location.pathname === '/contacts') setActiveTab(tabs[6].id);
-  }, [location]);
+  const lastPathname = window.location.pathname + window.location.hash;
 
   return (
     <Nav>
@@ -86,7 +44,7 @@ export function Navigation({ tabId }: INavigationProps) {
           key={tab.id}
           onClick={() => dismissMenu && dismissMenu()}
         >
-          {activeTab === tab.id && (
+          {lastPathname === tab.to && (
             <NavTab
               layoutId={tabId}
               transition={{ type: 'spring', bounce: 0.2, duration: 0.6 }}
